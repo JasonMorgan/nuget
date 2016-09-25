68a6f79a93fef7a760e14cb5df4bee2315f4c8fd
Jason Morgan <jmorgan@f9vs.com>
2016-09-24 20:04:42 -0400

    Added latest powershell cookbook to metadata.

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ license          'All rights reserved'
 description      'Bootstraps the Nuget DSC module and provides chef resources based off of  that module.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.8'
+depends          'powershell', '~>5.0.0'
